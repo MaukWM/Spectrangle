2ba5dc4c100b606dc508b4c5f656a1ae5dfac00d
gerryflap <gerry2211@gmail.com>
2018-12-16 18:43:42 +0100

    Fixes

--- game.py
+++ game.py
@@ -2,16 +2,21 @@ import random
 
 import agents.agent
 import state
+from agents.random_agent import RandomAgent
+
 
 def play_game(agents):
     s = state.State(4)
     agent = 0
     for i in range(len(agents)):
-        agents[i].on_game_start()
+        agents[i].on_game_start(s)
     while True:
-        s.step(agents[agent].get_move(), agent)
-        agent += 1 % len(agents)
+        s.step(agents[agent].get_move(s), agent)
+        agent =  (agent + 1) % len(agents)
 
 
 if __name__=="__main__":
-    pass
+    play_game([
+        RandomAgent(0),
+        RandomAgent(1)
+    ])
