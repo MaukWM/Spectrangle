c608777f5fb6e2f51a4a28f06e417d6e5f2b3289
Mauk <maukmul@gmail.com>
2018-12-16 13:10:34 +0100

    board generation

--- state.py
+++ state.py
@@ -1,17 +1,20 @@
 
 class Tile(object):
-    def __init__(self, bonus: int, points_up: bool):
+    def __init__(self, points_up: bool, bonus: int = 1):
         self.bonus = bonus
         self.contents = None
         self.owner = None
         self.points_up = points_up
+
+    def __repr__(self):
+        return str(self.contents) + str(self.bonus)
 
 
 class State(object):
     def __init__(self, state=None):
         if state is None:
             # Build new board
-            self.board = [[], []]
+            self.board = self.generate_board()
             # Init players and hands
             self.hands = tuple()
             self.in_bag = list()
@@ -45,7 +48,33 @@ class State(object):
         pass
 
     def generate_board(self):
-        pass
+        bonus_tiles = [(1, 1, 3),
+                       (3, 1, 2),
+                       (3, 2, 4),
+                       (3, 4, 4),
+                       (3, 5, 2),
+                       (4, 4, 4),
+                       (5, 1, 3),
+                       (5, 5, 2),
+                       (5, 9, 3)]
+        board = []
+        for i in range(6):
+            row = []
+            row_length = 1 + i*2
+            for j in range(row_length):
+                # Get potential bonus
+                (x, y) = (i, j)
+                bonus = 1
+                for k in range(len(bonus_tiles)):
+                    if bonus_tiles[k][0] == x and bonus_tiles[k][1] == y:
+                        bonus = bonus_tiles[k][2]
+                if j%2 == 0:
+                    row.append(Tile(points_up=True, bonus=bonus))
+                else:
+                    row.append(Tile(points_up=False, bonus=bonus))
+            board.append(row)
+        return board
+
 
     def calculate_score(self):
         # For all players
@@ -60,3 +89,8 @@ class State(object):
 
     def deepcopy(self):
         pass
+
+
+state = State()
+print(state.board)
+
